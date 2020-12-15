4a288c08e74dd473cbd13eb412a96f85500fe921
tomas1978 <tomas.nilsson78@gmail.com>
2020-12-15 09:18:30 +0100

    Problem 4 part 1 solved!

--- problem4/adventOfCode2020day4.py
+++ problem4/adventOfCode2020day4.py
@@ -1,4 +1,4 @@
-#Day 4 part 1, not yet correct
+#Day 4 part 1, answer: 210
 
 validPassports = 0
 lines = []      #lines read from the file
@@ -7,16 +7,16 @@ entries = []    #for complete lines of passport entries
 file = open('input.txt', 'r')
 
 for line in file:
-    if (line!="\n"):
+    if(line!="\n"):
         lines.append(line.strip())
     else:
-        lines.append("\n")
+        lines.append(line)
 file.close()
 
 #Remove newlines to create entries with all data for
 #one passport on a single line
 str=""
-for i in range(1,len(lines)):
+for i in range(0,len(lines)):
     if(lines[i]!="\n"):
         str+=lines[i]
     else:
@@ -25,17 +25,14 @@ for i in range(1,len(lines)):
 
 #Check and count valid passports
 for i in range(0,len(entries)):
-    if(entries[i].find("byr",0,len(entries[i])) and 
-       entries[i].find("iyr",0,len(entries[i])) and
-       entries[i].find("eyr",0,len(entries[i])) and
-       entries[i].find("hgt",0,len(entries[i])) and
-       entries[i].find("hcl",0,len(entries[i])) and
-       entries[i].find("ecl",0,len(entries[i])) and
-       entries[i].find("pid",0,len(entries[i]))):
+    if(entries[i].find("byr",0,len(entries[i]))!=-1 and 
+       entries[i].find("iyr",0,len(entries[i]))!=-1 and
+       entries[i].find("eyr",0,len(entries[i]))!=-1 and
+       entries[i].find("hgt",0,len(entries[i]))!=-1 and
+       entries[i].find("hcl",0,len(entries[i]))!=-1 and
+       entries[i].find("ecl",0,len(entries[i]))!=-1 and
+       entries[i].find("pid",0,len(entries[i]))!=-1):
         validPassports+=1
 
 print("Number of valid passports: ")
 print(validPassports)
-                    
-
-
